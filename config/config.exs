64eefe8a1ab6bfabfd44308b9538200d5ff8f48e
Eduardo B. Alexandre <sezdocs@github.com>
2024-01-22 13:00:01 -0300

    Add repo_2

--- config/config.exs
+++ config/config.exs
@@ -1,9 +1,16 @@
 import Config
 
-config :multi_repo, ecto_repos: [MultiRepo.Repo]
+config :multi_repo, ecto_repos: [MultiRepo.Repo, MultiRepo.Repo2]
 
 config :multi_repo, MultiRepo.Repo,
   database: "multi_repo",
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  port: "5432"
+
+config :multi_repo, MultiRepo.Repo2,
+  database: "multi_repo_2",
   username: "postgres",
   password: "postgres",
   hostname: "localhost",
---
+++ lib/multi_repo/repo_2.ex
@@ -0,0 +1,3 @@
+defmodule MultiRepo.Repo2 do
+  use AshPostgres.Repo, otp_app: :multi_repo
+end
